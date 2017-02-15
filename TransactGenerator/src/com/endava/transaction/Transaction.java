f3124c43e02afff03ee7900da07b61f5dfe2ef6b
victorangelo <victorangh@gmail.com>
2017-02-15 11:17:09 +0200

    Add files via upload

--- TransactGenerator/src/com/endava/transaction/Transaction.java
+++ TransactGenerator/src/com/endava/transaction/Transaction.java
@@ -12,38 +12,44 @@ public class Transaction {
 	private double transactionValue;
 	private Account sourceAccount;
 	private Account destinationAccount;
-	
+
 	public double getTransactionValue() {
 		return transactionValue;
 	}
+
 	public void setTransactionValue(double transactionValue) {
 		this.transactionValue = transactionValue;
 	}
+
 	public Account getSourceAccount() {
 		return sourceAccount;
 	}
+
 	public void setSourceAccount(Account sourceAccount) {
 		this.sourceAccount = sourceAccount;
 	}
+
 	public Account getDestinationAccount() {
 		return destinationAccount;
 	}
+
 	public void setDestinationAccount(Account destinationAccount) {
 		this.destinationAccount = destinationAccount;
 	}
-	
+
 	/**
 	 * Returns true if the sourceAccount and destinationAccount are different.
+	 * 
 	 * @return boolean
 	 */
-	public boolean validateTransaction(){
-		
-		if(this.sourceAccount != null || this.destinationAccount != null || 
-				(this.sourceAccount.getAccountID().equals(this.destinationAccount.getAccountID()) && 
-				this.sourceAccount.getBankID().equals(this.destinationAccount.getBankID()))){
-			return false;
+	public boolean validateTransaction() {
+
+		if (this.sourceAccount != null && this.destinationAccount != null
+				&& !(this.sourceAccount.getAccountID().equals(this.destinationAccount.getAccountID())
+						&& this.sourceAccount.getBankID().equals(this.destinationAccount.getBankID()))) {
+			return true;
 		}
-		
-		return true;
+
+		return false;
 	}
 }
